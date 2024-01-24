dcb70b7b90cdf02c3e2aef5c4b551dc6fd9a0fc2
AshishSharma1221 <ashishsharmaashish97@gmail.com>
2024-01-23 22:09:28 -0500

    ReadMe updated with Feature list

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+// npm i @react-navigation/bottom-tabs react-native-elements
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { Icon } from 'react-native-elements';
+function HomeScreen() {
+return (
+<View style={styles.container}>
+<Text style={styles.text}>Home Data</Text>
+</View>
+);
+}
+function SearchScreen() {
+return (
+<View style={styles.container}>
+<Text style={styles.text}>Search Data </Text>
+</View>
+);
+}
+function ProfileScreen() {
+return (
+<View style={styles.container}>
+<Text style={styles.text}>Profile Data</Text>
+</View>
+);
+}
+const Tab = createBottomTabNavigator();
+export default function ReactNavigationBottomTabs() {
+return (
+  <NavigationContainer>
+<Tab.Navigator
+tabBarOptions={
+{
+// Default Color is blue you can change it by following props
+// activeTintColor: '#ff4757',
+// inactiveTintColor: '#ff6b81',
+// Default Background Color is white you can change it by following props
+// activeBackgroundColor: '#ced6e0',
+// inactiveBackgroundColor: '#ced6e0',
+}
+}
+>
+<Tab.Screen
+name='Home'
+component={HomeScreen}
+options={{
+tabBarIcon: ({ color, size }) => (
+<Icon name='home' color={color} size={size} />
+),
+}}
+/>
+<Tab.Screen
+name='Search'
+component={SearchScreen}
+options={{
+tabBarIcon: ({ color, size }) => (
+<Icon name='message' color={color} size={size} />
+),
+}}
+/>
+<Tab.Screen
+name='Profile'
+component={ProfileScreen}
+options={{
+tabBarIcon: ({ color, size }) => (
+<Icon name='person' color={color} size={size} />
+),
+}}
+/>
+</Tab.Navigator>
+</NavigationContainer>
+);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+container: {
+flex: 1,
+alignItems: 'center',
+justifyContent: 'center',
+},
+text: {
+fontSize: 40,
+fontWeight: 'bold',
+},
 });
--- README.md
+++ README.md
@@ -5,6 +5,13 @@ Ashish Sharma,
 Pawandeep Singh,
 Bala Gayathri
 
+The app contains following features:
+1) Multi screen feature using tab navigation.
+2) The app contains Home Screen to showcase the music/Songs, Search Screen with a search bar to search the song and a profile screen to update the personal details like Profile Picture, Name, etc.
+3) This app is using an API which is used to fetch songs from the Deezer API.
+4) User Authentication is used for security purposes.
+
+
 
 
 
