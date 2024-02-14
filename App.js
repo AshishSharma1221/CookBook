7d7ea86f2b0530707ff985fbe29b94affa1759ec
AshishSharma1221 <ashishsharmaashish97@gmail.com>
2024-02-13 22:18:17 -0500

    Updated App.js for Tabs navigation

---
+++ App.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import RecipeListScreen from './RecipeListScreen';
+import RecipeDetailsScreen from './RecipeDetailsScreen';
+import CategoriesScreen from './CategoriesScreen'; 
+import { useTheme } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
+const Stack = createStackNavigator();
+const MaterialBottomTab = createMaterialBottomTabNavigator();
+
+const RecipeListStack = () => {
+  const theme = useTheme();
+  return (
+    <Stack.Navigator
+      initialRouteName="RecipeList"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: theme.colors.primary,
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}
+    >
+      <Stack.Screen
+        name="RecipeList"
+        component={RecipeListScreen}
+        options={{ title: 'CookBook' }}
+      />
+      <Stack.Screen
+        name="RecipeDetails"
+        component={RecipeDetailsScreen}
+        options={({ route }) => ({ title: route.params.recipe.strMeal })}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const App = () => {
+  const theme = useTheme();
+  return (
+    <NavigationContainer>
+      <MaterialBottomTab.Navigator
+        initialRouteName="Recipes"
+        shifting={true}
+        barStyle={{ backgroundColor: theme.colors.primary }}
+      >
+        <MaterialBottomTab.Screen
+          name="Recipes"
+          component={RecipeListStack}
+          options={{
+            tabBarIcon: 'home',
+            tabBarLabel: 'Recipes',
+          }}
+        />
+        <MaterialBottomTab.Screen
+          name="Categories"
+          component={CategoriesScreen}
+          options={{
+            tabBarIcon: ({ color }) => <Icon name="bars" size={20} color={color} />,
+            tabBarLabel: 'Categories',
+          }}
+        />
+      </MaterialBottomTab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
