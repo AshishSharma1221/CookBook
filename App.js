cc4f06317e0e42518a86cad181ff5f5e84fa33a5
AshishSharma1221 <ashishsharmaashish97@gmail.com>
2024-03-28 00:03:21 -0400

    Closing Issue #10 User Profiles as done.

--- App.js
+++ App.js
@@ -9,6 +9,8 @@ import CategoriesScreen from './CategoriesScreen';
 import FilteredRecipes from './FilteredRecipes';
 import SearchScreen from './SearchScreen';
 import PhotoGalleryScreen from './PhotoGalleryScreen';
+import SignInScreen from './SignInScreen';
+import UserProfileScreen from './UserProfileScreen';
 import { useTheme } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
@@ -49,6 +51,30 @@ const RecipeListStack = () => {
   );
 };
 
+const AuthStack = () => {
+  const theme = useTheme();
+  return (
+    <Stack.Navigator
+      initialRouteName="SignIn"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: theme.colors.primary,
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}
+    >
+      <Stack.Screen
+        name="SignIn"
+        component={SignInScreen}
+        options={{ title: 'Sign In' }}
+      />
+    </Stack.Navigator>
+  );
+};
+
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
   const theme = useTheme(); 
@@ -74,47 +100,74 @@ const App = () => {
       {isLoading ? (
         <SplashScreen />
       ) : (
-        <MaterialBottomTab.Navigator
-          initialRouteName="Recipes"
-          shifting={true}
-          barStyle={{ backgroundColor: theme.colors.primary }}
-        >
-          <MaterialBottomTab.Screen
-            name="Recipes"
-            component={RecipeListStack}
-            options={{
-              tabBarIcon: 'home',
-              tabBarLabel: 'Recipes',
-            }}
+        <Stack.Navigator>
+          <Stack.Screen
+            name="AuthStack"
+            component={AuthStack}
+            options={{ headerShown: false }}
           />
-          <MaterialBottomTab.Screen
-            name="SearchScreen"
-            component={SearchScreen}
-            options={{
-              tabBarIcon: 'magnify',
-              tabBarLabel: 'Search',
-            }}
+          <Stack.Screen
+            name="MainStack"
+            component={MainStack}
+            options={{ headerShown: false }}
           />
-          <MaterialBottomTab.Screen
-            name="Categories"
-            component={CategoriesScreen}
-            options={{
-              tabBarIcon: ({ color }) => <Icon name="bars" size={20} color={color} />,
-              tabBarLabel: 'Categories',
-            }}
-          />
-
-          <MaterialBottomTab.Screen
-            name="PhotoGallery"
-            component={PhotoGalleryScreen}
-            options={{
-              tabBarIcon: 'image',
-              tabBarLabel: 'Gallery',
-            }}
-          />
-        </MaterialBottomTab.Navigator>
+        </Stack.Navigator>
       )}
     </NavigationContainer>
+  );
+};
+
+const MainStack = () => {
+  const theme = useTheme();
+  return (
+    <MaterialBottomTab.Navigator
+      initialRouteName="Recipes"
+      shifting={true}
+      barStyle={{ backgroundColor: theme.colors.primary }}
+    >
+      <MaterialBottomTab.Screen
+        name="Recipes"
+        component={RecipeListStack}
+        options={{
+          tabBarIcon: 'home',
+          tabBarLabel: 'Recipes',
+        }}
+      />
+      <MaterialBottomTab.Screen
+        name="SearchScreen"
+        component={SearchScreen}
+        options={{
+          tabBarIcon: 'magnify',
+          tabBarLabel: 'Search',
+        }}
+      />
+      <MaterialBottomTab.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          tabBarIcon: ({ color }) => <Icon name="bars" size={20} color={color} />,
+          tabBarLabel: 'Categories',
+        }}
+      />
+
+      <MaterialBottomTab.Screen
+        name="PhotoGallery"
+        component={PhotoGalleryScreen}
+        options={{
+          tabBarIcon: 'image',
+          tabBarLabel: 'Gallery',
+        }}
+      />
+
+      <MaterialBottomTab.Screen
+        name="UserProfile"
+        component={UserProfileScreen}
+        options={{
+          tabBarIcon: 'account',
+          tabBarLabel: 'Profile',
+        }}
+      />
+    </MaterialBottomTab.Navigator>
   );
 };
 
---
+++ SignInScreen.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { app } from './firebase'; 
+
+const SignInScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState(null); // State for error message
+
+  const handleSignIn = async () => {
+    try {
+      const auth = getAuth(app);
+      await signInWithEmailAndPassword(auth, email, password);
+      navigation.navigate('MainStack', { screen: 'RecipeList' });
+    } catch (error) {
+      console.log('Error signing in:', error.message);
+      setErrorMessage('Wrong credentials, Please try again!'); // Setting error message
+    }
+  };  
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.card}>
+        <Text style={styles.cardTitle}>Sign In</Text>
+        <Text>Email:</Text>
+        <TextInput
+          style={styles.input}
+          value={email}
+          onChangeText={setEmail}
+          autoCapitalize="none"
+        />
+        <Text>Password:</Text>
+        <TextInput
+          style={styles.input}
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        {errorMessage && ( // Conditionally rendering error message
+          <Text style={styles.errorMessage}>{errorMessage}</Text>
+        )}
+        <Button title="Sign In" onPress={handleSignIn} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor:'pink'
+  },
+  card: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    backgroundColor:'lightgrey',
+    padding: 20,
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+  cardTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+    marginBottom: 10,
+    padding: 10,
+    paddingHorizontal: 'auto'
+  },
+  errorMessage: {
+    color: 'red',
+    marginBottom: 10,
+  },
+});
+
+export default SignInScreen;
